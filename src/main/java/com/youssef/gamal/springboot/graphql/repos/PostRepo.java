9d46364ccf6133a60427d1b1db69ce2800c91445
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-07-14 11:46:41 +0300

    added repos for all entities

---
+++ src/main/java/com/youssef/gamal/springboot/graphql/repos/PostRepo.java
@@ -0,0 +1,9 @@
+package com.youssef.gamal.springboot.graphql.repos;
+
+import com.youssef.gamal.springboot.graphql.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepo extends JpaRepository<Post, Long> {
+}
